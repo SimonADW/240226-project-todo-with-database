1d376a070c18cebcf40d25b2d90aa637bb5f6088
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-02-26 22:55:37 +0100

    Initialize and setup webpack bundle

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/js/app.js", 
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	watch: true
+};
+
+
