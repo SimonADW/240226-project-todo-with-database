b18c72e591abbb42e60b45b0d39d79f7dc38681a
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-02-27 11:57:26 +0100

    Add auth and log in/out, display sections accordingly

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,13 @@
-import { validateForm, validateDescription } from "./validateForm";
+import { firebaseConfig } from "./firebase.config";
+
+import {initializeApp} from "firebase/app";
+
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from "firebase/auth";
+
+
+initializeApp(firebaseConfig);
+
+
 
 //	DOM CONTENT TARGETS
 	// SIGN IN/UP SECTIONS
@@ -19,25 +28,34 @@ const titleError = document.querySelector(".title-error");
 const dateError = document.querySelector(".date-error");
 const descriptionError = document.querySelector(".description-error");
 
+const landingSection = document.querySelector(".landing-section");
+
 
 // SIGN UP/IN EVENTLISTENERS
 
 signInButton.addEventListener("click", ()=> {
+	signUpSection.style.display = "none";	
 	signInSection.style.display = "flex";
+	landingSection.style.display = "none";
 })
 
 signUpButton.addEventListener("click", ()=> {
+	signInSection.style.display = "none";	
 	signUpSection.style.display = "flex";
+	landingSection.style.display = "none";
 })
 
 closeButtons.forEach((button)=>{
 	button.addEventListener("click", (event)=> {
 		event.preventDefault();
 		event.currentTarget.parentElement.style.display = "none";
+		checkUserStatus();
 	});
 });
 
 // VALIDATE FORM
+
+import { validateForm, validateDescription } from "./validateForm";
 
 submitTodoButton.addEventListener("click", (event)=>{
 	event.preventDefault();
@@ -45,9 +63,98 @@ submitTodoButton.addEventListener("click", (event)=>{
 	if (formErrorStatus()){
 		return
 	} else {
-		console.log("Submitted")
+		console.log("Submitted");
 	}
 });
 
 validateDescription(descriptionInput, descriptionError, charCounter);
+
+
+// AUTHENTICATION ---------------------------------------------------
+
+const authService = getAuth();
+
+// SIGN UP USERS
+const signUpEmail = document.querySelector(".signup-form__email");
+const signUpPassword = document.querySelector(".signup-form__password");
+const signUpSubmitButton = document.querySelector(".signup-submit-button");
+
+const signUpUser = ()=> {
+	const userEmail = signUpEmail.value;
+	const userPassword = signUpPassword.value;
+	createUserWithEmailAndPassword(authService, userEmail, userPassword)
+	.then((cred)=>{
+		console.log(cred);
+		console.log("Account created!");
+	})
+	.catch((error)=> {
+		console.log(error.message);
+	})
+}
+
+signUpSubmitButton.addEventListener("click", (event)=> {
+	event.preventDefault();
+	signUpUser();
+	signUpSection.style.display = "none";	
+});
+
+// SIGN IN USERS
+
+const signInEmail = document.querySelector(".sign-in-form__email");
+const signInPassword = document.querySelector(".sign-in-form__password");
+const signInSubmitButton = document.querySelector(".sign-in-submit-button");
+
+signInSubmitButton.addEventListener("click", (event)=> {
+	event.preventDefault();
+	const userEmail = signInEmail.value;
+	const userPassword = signInPassword.value;
+	signInWithEmailAndPassword(authService, userEmail, userPassword)
+	.then(()=> {
+		console.log("Signed in ✔︎");				
+		signInSection.style.display = "none";	
+
+	})
+	.catch((error)=> console.log(error.message));
+})
+
+// SIGN OUT USERS
+
+const signOutUserButton = document.querySelector(".header__signout-button");
+
+const signOutUser = ()=> {
+	signOut(authService)
+	.then(()=> {
+		console.log("Signed out 👋");
+	})
+	.catch((error)=> console.log(error.message));
+}
+
+signOutUserButton.addEventListener("click", ()=> {
+	signOutUser();
+})
+
+// CHECK USER AUTH-STATUS
+function checkUserStatus() {	
+	const addTodoSection = document.querySelector(".add-todo-section");
+	const displayTodoSection = document.querySelector(".display-todos-section");
+	
+
+	onAuthStateChanged(authService, (user)=> {
+		if(user) {
+			addTodoSection.style.display = "flex";
+			displayTodoSection.style.display = "block";
+			landingSection.style.display = "none";
+		} else {	
+			addTodoSection.style.display = "none";
+			displayTodoSection.style.display = "none";
+			landingSection.style.display = "block";
+		};
+	});
+};
+
+checkUserStatus();
+
+
+
+
 
---
+++ src/js/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDssAz7UCQgS0OxvZ2_Xl9HH4CeAvp9Ajw",
+	authDomain: "todo-or-not-todos.firebaseapp.com",
+	projectId: "todo-or-not-todos",
+	storageBucket: "todo-or-not-todos.appspot.com",
+	messagingSenderId: "1095492163712",
+	appId: "1:1095492163712:web:ba6e7b9668b596963eae65"
+  };
+
+  export {firebaseConfig}
